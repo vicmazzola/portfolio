6edeb8f6610d7d110fb9e07ff3bc46ee969f9d6f
Victor Mazzola <victormazzola2019@gmail.com>
2024-12-02 08:01:17 -0300

    remove aos.js and add animate.css

--- js/app.js
+++ js/app.js
@@ -7,16 +7,10 @@ function navigateTo(sectionId) {
     const targetSection = document.getElementById(sectionId);
     if (targetSection) {
         targetSection.classList.remove('d-none');
+
     }
 }
 
 // Mostrar Home por padrão ao carregar a página
 navigateTo('home');
 
-// AOS.js
-document.addEventListener("DOMContentLoaded", () => {
-    AOS.init({
-        duration: 1000, // Duração das animações em ms (ajustável)
-        once: true      // Executar animação apenas uma vez (ou false para repetir ao scroll)
-    });
-});
